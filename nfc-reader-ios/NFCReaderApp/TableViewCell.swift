a8caa0bbae6ab3e64564ece0bb23149c288b2a70
Mahesh Lad <mlad.mahesh.lad@gmail.com>
2019-04-02 20:05:21 +0100

    initial commit

---
+++ nfc-reader-ios/NFCReaderApp/TableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewCell.swift
+//  NFCReaderApp
+//
+//  Created by Mahesh Lad on 02/04/2019.
+//  Copyright © 2019 Mahesh Lad. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    static let identifier = "TableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
---
+++ read card led on.ino
@@ -0,0 +1,58 @@
+//read card led on
+
+#include "SPI.h"
+#include "MFRC522.h"
+
+#define SS_PIN 10
+#define RST_PIN 9
+#define LED_PIN A0
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+MFRC522::MIFARE_Key key;
+
+void setup() {
+    Serial.begin(9600);
+    SPI.begin();
+    rfid.PCD_Init();
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial())
+    return;
+
+  // Serial.print(F("PICC type: "));
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  // Serial.println(rfid.PICC_GetTypeName(piccType));
+
+  // Check is the PICC of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    Serial.println(F("Your tag is not of type MIFARE Classic."));
+    return;
+  }
+
+  String strID = "";
+  for (byte i = 0; i < 4; i++) {
+    strID +=
+    (rfid.uid.uidByte[i] < 0x10 ? "0" : "") +
+    String(rfid.uid.uidByte[i], HEX) +
+    (i!=3 ? ":" : "");
+  }
+  strID.toUpperCase();
+
+  Serial.print("Tap card key: ");
+  Serial.println(strID);
+
+  //  LED   strID   -one card to turn on other to tutn off
+  if (strID.indexOf("79:54:A5:55") >= 0) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
+
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
+}
